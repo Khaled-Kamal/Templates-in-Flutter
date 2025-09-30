916df177a7fd62b6c94dca263929c00d9712c97c
Khaled Kamal <KhaledKamal1555@gmail.com>
2025-09-30 18:22:21 +0300

    Create Slide Animation.dart

---
+++ Slide Animation.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 1),
+      end: Offset.zero,          
+    ).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SlideTransition(
+          position: _slideAnimation,
+          child: Image.asset(
+            'assets/foodie_splash.png',
+            width: 167,
+          ),
+        ),
+      ),
+    );
+  }
+}
