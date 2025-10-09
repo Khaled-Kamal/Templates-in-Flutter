198eef7ac2e8708dae009123fb4666bfc2ff220b
Khaled Kamal <KhaledKamal1555@gmail.com>
2025-10-09 15:40:14 +0300

    Update and rename TEST.dart to Test_Code_inProjects.dart

---
+++ Test_Code_inProjects.dart
@@ -0,0 +1,53 @@
+ SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: SizedBox(
+              height: 60,
+
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                separatorBuilder: (_, __) => SizedBox(width: 15),
+                itemBuilder: (context, index) {
+                  final item = categories[index];
+                  bool isSelected = selectIndex == index;
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectIndex = index;
+                      });
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(milliseconds: 400),
+                      width: 131,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: isSelected
+                            ? Color(0xffFCE6D9)
+                            : Color(0xffF9F1DA),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Image.asset(item.image, width: 30),
+                            SizedBox(width: 5),
+                            Text(
+                              item.title,
+                              style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
