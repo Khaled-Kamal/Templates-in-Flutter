5a34bf1a9848226c30124d99582ddad62d4e4a3a
Khaled Kamal <KhaledKamal1555@gmail.com>
2025-10-09 15:06:43 +0300

    Update TEST.dart

--- TEST.dart
+++ TEST.dart
@@ -27,7 +27,6 @@ class _FoodCarouselState extends State<FoodCarousel> {
               child: Stack(
                 clipBehavior: Clip.none,
                 children: [
-                  // الكارد
                   Container(
                     margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 40),
                     padding: const EdgeInsets.symmetric(vertical: 60, horizontal: 16),
